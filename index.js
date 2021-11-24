c9e3ebaa236b9c8068b279521f37a6b9a91a852b
ritchey0713 <ritchey0713@gmail.com>
2021-11-24 11:21:34 -0500

    commit tree 63e7f1c5afb7a6e9e9740f65ce280edf83c24696

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+// 2 possible return values
 
+// the arg coming in is an array
+
+// const array1 = [5, 12, 8, 130, 44];
+
+// const found = array1.find(element => element > 10);
+
+// myObj = myArrayOfObjects.find(obj => obj.prop === 'something');
+
+const superbowlWin = (array) => {
+  const obj = array.find((record) => record.result === "W");
+  if (obj !== undefined) {
+    return obj.year;
+  } else {
+    return undefined;
+  }
+};
